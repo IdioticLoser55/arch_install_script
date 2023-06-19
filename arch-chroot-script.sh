0c651000d6384c933e8456bba701e8d898ace1ed
IdioticLoser55 <IdioticLoser55@gmail.com>
2023-06-19 21:06:36 +0000

    enabled grub. Made package choices for plasma and kde-applications.

--- arch-chroot-script.sh
+++ arch-chroot-script.sh
@@ -11,9 +11,17 @@ HOSTNAME="IdiotsLaptop23"
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
 pacman -Syu
 
-INSTALL_PACKAGES="ntfs-3g ufw xorg xf86-video-amdgpu mesa lib32-mesa nvidia nvidia-utils lib32-nvidia-utils plasma kde-applications sddm noto-fonts firefox"
-
+INSTALL_PACKAGES="ntfs-3g ufw xorg xf86-video-amdgpu mesa lib32-mesa nvidia nvidia-utils lib32-nvidia-utils sddm firefox"
 pacman -S $INSTALL_PACKAGES
+
+# have to make choices for plasma.
+INSTALL_PACKAGES="plasma pipewire-jack wireplumber noto-fonts phonon-qt5-vlc"
+pacman -S --noconfirm $INSTALL_PACKAGES
+
+# have to make choices for kde-applications
+INSTALL_PACKAGES="kde-applications python-pyqt5 fcron tesseract-data-eng"
+pacman -S --noconfirm $INSTALL_PACKAGES
+
 
 ln -sf "/usr/share/zoninfo/${TIME_ZONE_INFO}" "/etc/localtime"
 hwclock --systohc
@@ -33,16 +41,16 @@ sed -i "s/block/keyboard keymap block encrypt lvm2/g" "/etc/mkinitcpio.conf"
 mkinitcpio -P
 
 ## DON'T RUN THIS WHEN TESTING. BREAKS GRUB.
-#
-#mkdir -p /boot/EFI/GRUB
-#grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-#
-#echo "s/GRUB_CMDLINE_LINUX_DEFAULT=\"/GRUB_CMDLINE_LINUX_DEFAULT=\"cryptdevice=UUID=\"${CRYPT_UUID}\":cryptlvm:allow-discards root=${ROOT_PATH//\//\\/} swap=${SWAP_PATH//\//\\/} /g"
-#sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=\"/GRUB_CMDLINE_LINUX_DEFAULT=\"cryptdevice=UUID=\"${CRYPT_UUID}\":cryptlvm:allow-discards root=${ROOT_PATH//\//\\/} swap=${SWAP_PATH//\//\\/} /g" "/etc/default/grub"
-#sed -i "s/#GRUB_ENABLE_CRYPTODISK=y/GRUB_ENABLE_CRYPTODISK=y/g" "/etc/default/grub"
-#sed -i "s/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/g" "/etc/default/grub"
-#
-#grub-mkconfig -o /boot/grub/grub.cfg
+
+mkdir -p /boot/EFI/GRUB
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+
+echo "s/GRUB_CMDLINE_LINUX_DEFAULT=\"/GRUB_CMDLINE_LINUX_DEFAULT=\"cryptdevice=UUID=\"${CRYPT_UUID}\":cryptlvm:allow-discards root=${ROOT_PATH//\//\\/} swap=${SWAP_PATH//\//\\/} /g"
+sed -i "s/GRUB_CMDLINE_LINUX_DEFAULT=\"/GRUB_CMDLINE_LINUX_DEFAULT=\"cryptdevice=UUID=\"${CRYPT_UUID}\":cryptlvm:allow-discards root=${ROOT_PATH//\//\\/} swap=${SWAP_PATH//\//\\/} /g" "/etc/default/grub"
+sed -i "s/#GRUB_ENABLE_CRYPTODISK=y/GRUB_ENABLE_CRYPTODISK=y/g" "/etc/default/grub"
+sed -i "s/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false/g" "/etc/default/grub"
+
+grub-mkconfig -o /boot/grub/grub.cfg
 
 
 echo "root:$USER_PASSWORD" | chpasswd
